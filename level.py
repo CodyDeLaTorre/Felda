058846a9a50ed447a90699fe45ce319730e5bda4
CODY DE LA TORRE <cdelatorre414@gmail.com>
2023-03-24 21:19:58 -0600

    added more stuff

--- level.py
+++ level.py
@@ -1,4 +1,7 @@
 import pygame
+from settings import *
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -10,5 +13,13 @@ class Level:
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
 
+    def create_map(self):
+        for row, row_index in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'X':
+                    Tile((x,y)[self.visible_sprites])
+
     def run(self):
-        pass
+        self.visible_sprites.draw(self.display_surface)
