8d4ae404e666f9595933f70cb3fe256399740172
CODY DE LA TORRE <cdelatorre414@gmail.com>
2023-04-03 08:18:53 -0600

    added a player

--- game/level.py
+++ game/level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -21,7 +22,11 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'X':
-                    Tile((x,y),[self.visible_sprites])
+                    Tile((x,y),[self.visible_sprites, self.obstacles_sprites])
+                if col == 'p':
+                    self.player = Player((x,y),[self.visible_sprites])
 
     def run(self):
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
